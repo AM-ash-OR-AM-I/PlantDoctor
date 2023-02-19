f4cf93259f7f58dac0011d57a4fbc92102571812
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2023-02-19 20:41:27 +0530

    feat:Add more result data

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 import base64
 import io
+import json
 import logging
 from flask import Flask, request, jsonify
 
@@ -8,13 +9,28 @@ app = Flask("plant")
 from PIL import Image
 
 
+with open("data.json", "r") as f:
+    data_json = json.load(f)
+
 @app.post('/')
 def make_prediction():
     image = request.get_json()['image']
     image_data = base64.b64decode(image)
     image = Image.open(io.BytesIO(image_data))
     results = predict(image)
-    return jsonify(disease=results)
+    data = data_json.get(results)
+    if data is None:
+        return jsonify(disease=results)
+    else:
+        return jsonify(
+            disease=results,
+            causes=data.get("Causes"),
+            prevention=data.get("Prevention"),
+            remedies=data.get("Remedies"),
+            fertilizer=data.get("Fertilizer"),
+
+        )
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001)
