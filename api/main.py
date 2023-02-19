d72fa35a1625fe0761ce42f48861f28e84d02e2e
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2023-02-19 11:34:33 +0530

    fix: minor fix

--- api/main.py
+++ api/main.py
@@ -14,7 +14,7 @@ def make_prediction():
     image_data = base64.b64decode(image)
     image = Image.open(io.BytesIO(image_data))
     results = predict(image)
-    return jsonify(output1=results[0], output2=results[1])
+    return jsonify(disease=results)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001)
